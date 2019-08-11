1535e6a5e5c1eab33c51c741f5e2d2c486b2255b
kiboy19 <51844832+kiboy19@users.noreply.github.com>
2019-08-10 23:00:29 -0700

    Create apt.sh

---
+++ apt.sh
@@ -0,0 +1,15 @@
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install php -y
+apt install wget -y
+apt install figlet -y
+apt install toilet -y
+apt install python2 -y
+apt install python -y
+apt install ruby -y
+gem install lolcat -y
+apt install pip -y
+apt install pip2 -y
+pip2 install requests -y
+pip2 install termcolor -y
